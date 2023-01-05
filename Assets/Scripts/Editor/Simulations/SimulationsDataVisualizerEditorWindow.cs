fc1b182caa102d72086949b426e5dc1e75c95387
mnicolas94 <miguelcok27@gmail.com>
2023-01-05 10:00:43 -0500

    feat(simulations): add data results label and compute data button
    close #58, close #60

--- Assets/Scripts/Editor/Simulations/SimulationsDataVisualizerEditorWindow.cs
+++ Assets/Scripts/Editor/Simulations/SimulationsDataVisualizerEditorWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Simulations;
 using Simulations.DataExtractors;
 using TNRD;
@@ -12,10 +13,10 @@ namespace Editor.Simulations
 {
     public class SimulationsDataVisualizerEditorWindow : EditorWindow
     {
-        private VisualizationInputs _inputs;
-        private SerializedObject _inputsSerializedObject;
-        private SerializedProperty _extractorProperty;
-        private SerializedProperty _resultsProperty;
+        [SerializeField] private SerializableInterface<ISimulationDataExtractor> _extractor = new SerializableInterface<ISimulationDataExtractor>();
+        [SerializeField] private List<SimulationResult> _results = new List<SimulationResult>();
+
+        private Label _resultsLabel;
         
         [MenuItem("Simulations/Data visualization")]
         private static void ShowWindow()
@@ -23,48 +24,76 @@ namespace Editor.Simulations
             var window = GetWindow<SimulationsDataVisualizerEditorWindow>();
             window.titleContent = new GUIContent("Data visualization");
         }
-
-        private void OnEnable()
+        
+        private void CreateGUI()
         {
-            Debug.Log("OnEnable");
-            _inputs = CreateInstance<VisualizationInputs>();
-            _inputsSerializedObject = new SerializedObject(_inputs);
-            _extractorProperty = _inputsSerializedObject.FindProperty("_extractor");
-            _resultsProperty = _inputsSerializedObject.FindProperty("_results");
+            SetupUxml();
+
+            // input fields
+            var extractorsField = rootVisualElement.Q("ExtractorPropertyField");
+            var resultsField = rootVisualElement.Q("ResultsPropertyField");
+            var so = new SerializedObject(this);
+            extractorsField.Bind(so);
+            resultsField.Bind(so);
+            
+            // compute data fields
+            var computeButton = rootVisualElement.Q<Button>("ComputeButton");
+            computeButton.clicked += ComputeData;
+
+            // visualization fields
+            _resultsLabel = rootVisualElement.Q<Label>("ResultsLabel");
         }
 
-        private void CreateGUI()
+        private void SetupUxml()
         {
             var uxmlPath = "Assets/Scripts/Editor/Simulations/" +
                            "VisualizationEditorWindow/SimulationsDataVisualizerEditorWindow.uxml";
             VisualTreeAsset uiAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(uxmlPath);
             VisualElement ui = uiAsset.Instantiate();
             rootVisualElement.Add(ui);
+        }
+
+        private void ComputeData()
+        {
+            var results = _results.Select(simulationResult =>
+            {
+                var data = _extractor.Value.ExtractGamesData(simulationResult);
+                return (result: simulationResult, data);
+            });
             
-            /**
-             * inputs
-             * - extractor
-             * - simulations: list
-             * outputs
-             * - data foreach simulation (in a table)
-             */
-            var inputsLeftLayout = rootVisualElement.Q("InputsLayoutLeft");
-            var inputsRightLayout = rootVisualElement.Q("InputsLayoutRight");
+            VisualizeData(results.ToList());
+        }
+
+        private void VisualizeData(List<(SimulationResult, Dictionary<string, object>)> data)
+        {
+            var columnHeaders = new List<string> { "Names\t\t\t" };
+            columnHeaders.AddRange(data[0].Item2.Keys);
+            string columns = string.Join("\t", columnHeaders);
             
-            var extractorsField = new PropertyField(_extractorProperty);
-            var resultsField = new PropertyField(_resultsProperty);
-            extractorsField.Bind(_inputsSerializedObject);
-            resultsField.Bind(_inputsSerializedObject);
+            var strings = data.Select(tuple =>
+            {
+                var (simulationResult, simulationData) = tuple;
+                var dataValues = simulationData.Values;
+
+                var strData = string.Join(";\t\t", dataValues);
+
+                var str = $"{simulationResult.name}: \t{strData}";
+                return str;
+            }).ToList();
+
+            strings.Insert(0, columns);
             
-            inputsLeftLayout.Add(extractorsField);
-            inputsRightLayout.Add(resultsField);
+            _resultsLabel.text = string.Join("\n", strings);
         }
     }
 
-    [Serializable]
     public class VisualizationInputs : ScriptableObject
     {
         [SerializeField] private SerializableInterface<ISimulationDataExtractor> _extractor = new SerializableInterface<ISimulationDataExtractor>();
         [SerializeField] private List<SimulationResult> _results = new List<SimulationResult>();
+
+        public SerializableInterface<ISimulationDataExtractor> Extractor => _extractor;
+
+        public List<SimulationResult> Results => _results;
     }
 }
